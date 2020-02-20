354908b6ee627276c0d2bbdbfaa3562d6f67dfda
DA0HN <vegancraft13@gmail.com>
2020-02-20 19:37:18 -0400

    Futura implementacao para gerenciar leitura e escrita de arquivo

--- src/aurora/file/FileManager.java
+++ src/aurora/file/FileManager.java
@@ -1,8 +1,16 @@
 package aurora.file;
 
+import aurora.parser.ParsedPath;
+
 /*
  * @project aurora
  * @author Gabriel Honda on 20/02/2020
  */
 public class FileManager {
+
+    private ParsedPath data;
+
+    public FileManager(ParsedPath data) {
+        this.data = data;
+    }
 }
