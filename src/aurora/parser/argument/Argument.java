032f6e8d9da91741eb3f040e3c80a863d0fc8b75
DA0HN <vegancraft13@gmail.com>
2020-02-21 20:17:09 -0400

    Refatoracao do codigo, atualizacao da documentacao. Implementado criacao de .asm caso nao seja passado via argumento

--- src/aurora/parser/argument/Argument.java
+++ src/aurora/parser/argument/Argument.java
@@ -1,10 +1,12 @@
 package aurora.parser.argument;
 
+import aurora.file.AsmFile;
 import aurora.parser.ParsedPath;
 
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -14,13 +16,31 @@ import java.util.stream.Stream;
  */
 public class Argument {
 
-    /*
-     * criar .asm com o mesmo nome do arquivo .au
-     * */
     public static ParsedPath parseArgs(String[] args) {
-        final String aurora = ".au";
-        final String assembly = ".asm";
+        Path asmPath, auroraPath;
+        final String auroraExt = ".au";
+        final String asmExt = ".asm";
+
         // converte os argumentos para ArrayList para utilizar Stream
+        List<String> listArgs = activateFlags(args);
+
+        // assume-se que um arquivo aurora deve-se ter a extensao .au,
+        // procura dentro da lista uma string que termina com .au
+        Optional<String> _au = findFile(listArgs, arg -> arg.endsWith(auroraExt));
+
+        Optional<String> _asm = findFile(listArgs, arg -> arg.endsWith(asmExt));
+
+        auroraPath = Path.of(_au.orElseThrow(() -> new IllegalArgumentException("Não foi encontrado o arquivo do tipo" + auroraExt)));
+
+        asmPath = processAsmPath(auroraPath, _asm);
+
+        // retorna uma instancia de ParsedPath que possui a informacao do caminho
+        // dos arquivos de input e output
+        // que serao usados por outras classes
+        return new ParsedPath(auroraPath, asmPath);
+    }
+
+    private static List<String> activateFlags(String[] args) {
         List<String> listArgs = Arrays.asList(args);
 
         if(listArgs.contains("--all")) {
@@ -34,27 +54,21 @@ public class Argument {
                     .filter(arg -> listArgs.contains(arg) && arg.startsWith("--"))
                     .forEach(arg -> Flag.getFlag(arg).setValue(true));
         }
-        // assume-se que um arquivo aurora deve-se ter a extensao .au,
-        // procura dentro da lista uma string que termina com .au
-        Path pathAU = Path.of(findFile(listArgs,
-                                       arg -> arg.endsWith(aurora),
-                                       aurora
-        ));
-        Path pathASM = Path.of(findFile(listArgs,                       // lista que talvez contenha o caminho do arquivo
-                                        arg -> arg.endsWith(assembly),  // teste para encontrar o arquivo
-                                        assembly
-        ));                     // extensão para a mensagem de erro
-        // retorna uma instancia de ParsedPath que possui a informacao do caminho dos arquivos de input e output
-        // que serao usados por outras classes
-        return new ParsedPath(pathAU, pathASM);
+        return listArgs;
     }
-
-    private static String findFile(List<String> list, Predicate<String> constraint, String ext) {
+    private static Path processAsmPath(Path auroraPath, Optional<String> _asm) {
+        Path asmPath;
+        if(_asm.isEmpty()){
+            asmPath = AsmFile.create(auroraPath);
+        } else {
+            asmPath = Path.of(_asm.get());
+        }
+        return asmPath;
+    }
+    private static Optional<String> findFile(List<String> list, Predicate<String> constraint) {
         return list.stream()
                 .filter(constraint)     // executa o metodo de teste
-                .findFirst()            // recupera o primeiro resultado que é um Optional
-                // se o Optional for nulo lança a exceção
-                .orElseThrow(() -> new IllegalArgumentException("Não foi encontrado o arquivo do tipo" + ext));
+                .findFirst();           // recupera o primeiro resultado que é um Optional
     }
 
 }
