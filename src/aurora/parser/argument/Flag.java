4fea87e6a5d7f1deccfa122babeda52828e8d539
DA0HN <vegancraft13@gmail.com>
2020-02-22 15:12:42 -0400

    Atualizacao da documentacao

--- src/aurora/parser/argument/Flag.java
+++ src/aurora/parser/argument/Flag.java
@@ -5,7 +5,11 @@ package aurora.parser.argument;
  * @author Gabriel Honda on 19/02/2020
  */
 public enum Flag {
-
+    /*
+    * Enumerados sao Singletons por natureza, os valores das flags sao mantidos em toda
+    * a execucao da aplicacao, assim se o booleano for alterado
+    * essa alteracao sera permanente ate o final da execucao do programa
+    * */
     TOKENS(false, "--tokens"),
     SYNTACTIC(false,"--syntactic"),
     SEMANTIC(false, "--semantic"),
