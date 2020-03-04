e2568735b3cae81906cb2c208fae1b12a4a7e11b
Gabriel Honda <vegancraft13@gmail.com>
2020-03-04 11:32:32 -0400

    Classes complementares para o analisador léxico

--- src/aurora/analyzer/lexical/interfaces/BufferAnalyzer.java
+++ src/aurora/analyzer/lexical/interfaces/BufferAnalyzer.java
@@ -1,13 +1,11 @@
 package aurora.analyzer.lexical.interfaces;
 
-import aurora.analyzer.lexical.enums.Keyword;
-import aurora.analyzer.lexical.enums.Token;
+import aurora.analyzer.lexical.tokens.Keyword;
+import aurora.analyzer.lexical.tokens.Symbol;
+import aurora.analyzer.lexical.tokens.Token;
 
 import java.util.Optional;
 import java.util.function.Function;
-
-import static aurora.analyzer.lexical.interfaces.BufferAnalyzer.isIdentifier;
-import static aurora.analyzer.lexical.interfaces.BufferAnalyzer.isNumber;
 
 /*
  * @project aurora
@@ -15,31 +13,25 @@ import static aurora.analyzer.lexical.interfaces.BufferAnalyzer.isNumber;
  */
 public interface BufferAnalyzer extends Function<String, Optional<IToken>> {
 
-    static BufferAnalyzer isKeyword() {
-        return buffer -> Keyword.getValues()
-                .contains(buffer) ? Optional.of(Keyword.toEnum(buffer)) : Optional.empty();
+    static BufferAnalyzer keyword() {
+        return buffer -> AnalyzerService.isKeyword(buffer) ? Optional.of(Keyword.toEnum(buffer)) : Optional.empty();
     }
-    static BufferAnalyzer isIdentifier() {
-        return buffer -> Character.isDigit(buffer.charAt(0))
-                || buffer.chars().allMatch(Character::isLetterOrDigit)
+    static BufferAnalyzer identifier() {
+        return buffer -> AnalyzerService.isIdentifier(buffer)
                 ? Optional.of(Token.ID) : Optional.empty();
     }
-    static BufferAnalyzer isNumber() {
-        return buffer -> buffer.chars().allMatch(Character::isDigit)
+    static BufferAnalyzer number() {
+        return buffer -> AnalyzerService.isNumber(buffer)
                 ? Optional.of(Token.NUMBER) : Optional.empty();
     }
-
+    static BufferAnalyzer symbol() {
+        return buffer -> AnalyzerService.isSymbol(buffer)
+                ? Optional.of(Symbol.toEnum(buffer)) : Optional.empty();
+    }
     default BufferAnalyzer orElse(BufferAnalyzer other) {
         return buffer -> {
             Optional<IToken> result = other.apply(buffer);
             return result.or(() -> this.apply(buffer));
         };
     }
-
-    public static void main(String[] args) {
-        var buff = "aurora::init";
-        Optional<IToken> apply = BufferAnalyzer.isKeyword().orElse(isIdentifier())
-                .orElse(isNumber()).apply(buff);
-    }
-
 }
