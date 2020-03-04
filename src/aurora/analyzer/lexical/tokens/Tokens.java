541e8bed8e5a80c3889ace7cf09688bee9fbdff8
Gabriel Honda <vegancraft13@gmail.com>
2020-03-04 11:22:56 -0400

    Classe que encapsula a lista de tokens gerada pela classe Lexical

---
+++ src/aurora/analyzer/lexical/tokens/Tokens.java
@@ -0,0 +1,25 @@
+package aurora.analyzer.lexical.tokens;
+
+import aurora.analyzer.lexical.utils.TokenContainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @project aurora
+ * @author Gabriel Honda on 01/03/2020
+ */
+public class Tokens {
+    private static List<TokenContainer> tokens;
+
+    static {
+        tokens = new ArrayList<>();
+    }
+
+    public static void add(TokenContainer tk){
+        tokens.add(tk);
+    }
+    public static List<TokenContainer> get(){
+        return tokens;
+    }
+}
