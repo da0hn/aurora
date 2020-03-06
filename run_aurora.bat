17498faac9487d2c907c5b7de645663191ee1ac8
Gabriel Honda <vegancraft13@gmail.com>
2020-03-06 19:55:26 -0400

    Atualização do .bat
    dev_aurora.bat vem com o caminho do arquivo test.au enquanto o run_aurora.bat necessita da entrada de um arquivo para funcionar

---
+++ run_aurora.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem utf-8
+chcp 65001>nul
+set args=%*
+
+rem procura, os arquivos .java e compila
+
+set java_files=
+
+for /f "tokens=*" %%f in ('dir /b /s *.java') do (
+    set java_files=%%f !java_files!
+)
+
+javac %java_files% -d ./compiled/
+
+rem set parent_folder=%cd%
+set test_folder=%cd%\test
+
+cd compiled
+
+java aurora.app.Aurora %*
+
+endlocal
+exit /b /0
