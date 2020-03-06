4eb013051b8ca790df1f26b958d591604029b9fb
Gabriel Honda <vegancraft13@gmail.com>
2020-03-06 19:57:03 -0400

    Flag Readable e atualizações na documentação

--- src/aurora/analyzer/lexical/Lexical.java
+++ src/aurora/analyzer/lexical/Lexical.java
@@ -1,6 +1,8 @@
 package aurora.analyzer.lexical;
 
 import aurora.analyzer.lexical.interfaces.AnalyzerService;
+import aurora.analyzer.lexical.utils.LogLexical;
+import aurora.parser.Flag;
 
 import java.util.Collections;
 import java.util.List;
@@ -16,15 +18,15 @@ import static aurora.analyzer.lexical.interfaces.LinesParserService.splitBy;
  */
 public class Lexical {
     /*
-    * classe interna responsavel pelo controle das linhas,
-    * colunas e tamanho da linha
-    * */
+     * classe interna responsavel pelo controle das linhas,
+     * colunas e tamanho da linha
+     * */
     public static class Controls {
         private static Integer line = 1;
         private static Integer column = 1;
         private static Integer lineLength;
 
-        public static void setLineLength(List<String> line){
+        public static void setLineLength(List<String> line) {
             // recebe a linha atual e soma os caracteres de cada String inserida
             // na lista
             lineLength = line.stream()
@@ -32,67 +34,83 @@ public class Lexical {
                     .sum();
         }
 
-        public static void incrementColumn(){ column++;}
-        public static void incrementColumn(Integer column){
+        public static void incrementColumn() { column++;}
+
+        public static void incrementColumn(Integer column) {
             Controls.column += column;
         }
-        public static void incrementLine(){ line++;}
-        public static void resetColumn(){ column = 1;}
+
+        public static void incrementLine() { line++;}
+
+        public static void resetColumn() { column = 1;}
+
         public static Integer getColumn() {
             return column;
         }
+
         public static Integer getLine() {
             return line;
         }
+
         public static Integer getLineLength() {
             return lineLength;
         }
     }
 
-    public void analyze(List<String> lines){
-
+    public void analyze(List<String> lines) {
         // metodo splitBy() quebra a linha de acordo com o delimitador
         // o delimitador é mantido na lista criada para analise
         for(String line : lines) {
-            var parsedLines = splitBy("\\)")
-                    .andThen(splitBy("\\("))
-                    .andThen(splitBy("\\s"))
-                    .andThen(splitBy(";"))
-                    .andThen(splitBy("\""))
-                    .andThen(splitBy(">"))
-                    .andThen(splitBy("<"))
-                    .andThen(splitBy("="))
-                    .andThen(splitBy("\\+"))
-                    .andThen(splitBy("\\-"))
-                    .andThen(splitBy("\\*"))
-                    .andThen(splitBy("/"))
-                    .apply(Collections.singletonList(line));
+            try {
+                var parsedLines = splitBy("\\)")
+                        .andThen(splitBy("\\("))
+                        .andThen(splitBy("\\s"))
+                        .andThen(splitBy(";"))
+                        .andThen(splitBy("\""))
+                        .andThen(splitBy(">"))
+                        .andThen(splitBy("<"))
+                        .andThen(splitBy("="))
+                        .andThen(splitBy("\\+"))
+                        .andThen(splitBy("\\-"))
+                        .andThen(splitBy("\\*"))
+                        .andThen(splitBy("/"))
+                        .apply(Collections.singletonList(line));
 //            System.out.println(parsedLines + " size -> " + parsedLines.size());
-            analyzeLines(parsedLines);
-
-            incrementLine();
-            resetColumn();
+                analyzeLines(parsedLines);
+                incrementLine();
+                resetColumn();
+                if(Flag.READABLE.getValue()) {
+                    Thread.sleep(400);
+                }
+            }
+            catch(InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        LogLexical.message("Lexical OK");
     }
 
     private void analyzeLines(List<String> lines) {
+        // se a linha for considerada vazia sai do metodo
+        // uma linha e considerada vazia se possui apenas espaços em brancos
+        // ate o seu final
         if(isLineEmpty(lines)) return;
         // seta o tamanho total da linha
         setLineLength(lines);
         var it = lines.listIterator();
         // itera a lista que representa uma linha do arquivo .au
-        while(it.hasNext()){
+        while(it.hasNext()) {
             var current = it.next();
-            // teste de comentario
+            // teste se é comentario
             if(current.equals("/") && it.next().equals("/")) {
                 return;
             }
-            // teste de linha em branco
-            else if(current.equals(" ")){
+            // testa se é uma linha em branco
+            else if(current.equals(" ")) {
                 incrementColumn();
             }
-            // testa se e um comentario
-            else if(current.equals("\"")){
+            // testa se é um comentario
+            else if(current.equals("\"")) {
                 if(stringAnalyzer(it)) return;
             }
             // caso nao entre em nenhuma das classificacoes anteriores
