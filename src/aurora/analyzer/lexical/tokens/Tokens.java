0fe7e82df5d154e190636768c5af6d03357c0d17
Gabriel Honda <vegancraft13@gmail.com>
2020-03-06 20:47:05 -0400

    Melhoria na documentação

--- src/aurora/analyzer/lexical/tokens/Tokens.java
+++ src/aurora/analyzer/lexical/tokens/Tokens.java
@@ -9,6 +9,10 @@ import java.util.List;
  * @project aurora
  * @author Gabriel Honda on 01/03/2020
  */
+
+/* classe utilizada para encapsular a lista de tokens
+ * populada na classe Lexical
+ * */
 public class Tokens {
     private static List<TokenContainer> tokens;
 
