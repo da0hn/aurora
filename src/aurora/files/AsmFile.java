d56764700022ee37f3c3cc7b322fedfeb4cf3f98
Gabriel Honda <vegancraft13@gmail.com>
2020-03-06 20:45:54 -0400

    Otimização dos imports

--- src/aurora/files/AsmFile.java
+++ src/aurora/files/AsmFile.java
@@ -3,9 +3,7 @@ package aurora.files;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 /*
  * @project aurora
@@ -17,14 +15,13 @@ public class AsmFile {
      * caso o arquivo .asm não exista cria-se um novo
      * na mesma pasta que o .au e com o mesmo nome
      * */
-    public static Path create(Path path){
+    public static Path create(Path path) {
         try {
             var asm = new File(getDir(path)
                                        + "/"
                                        + getAsmName(extractAuroraName(path))
                                        + ".asm");
-            asmExists()
-                    .and(deleteAsmFile())
+            asmExists().and(deleteAsmFile())
                     .test(asm);
             System.out.println("O arquivo " + asm.getAbsoluteFile().getName() + " ja existe, " +
                                        "um novo arquivo .asm sera criado");
@@ -40,34 +37,41 @@ public class AsmFile {
     private static void isAsmFileCreated(File asm) throws IOException {
         boolean fileCreated = asm.createNewFile();
 
-        if( fileCreated ) {
+        if(fileCreated) {
             System.out.println("Arquivo " + asm.getAbsoluteFile().getName() + " criado com sucesso");
-        } else {
+        }
+        else {
             throw new IOException("O arquivo .asm nao foi criado.");
         }
     }
-    private static Predicate<File> asmExists(){
+
+    private static Predicate<File> asmExists() {
         return File::exists;
     }
-    private static Predicate<File> deleteAsmFile(){
+
+    private static Predicate<File> deleteAsmFile() {
         return File::delete;
     }
+
     private static String getAsmName(String inputName) {
         // remove a extensao do arquivo para ser utilizado na criacao do .asm
-        return inputName.substring(0,                       // percorre do inicio da string
-                                   inputName.indexOf(".")); // ate a localizacao do ponto ( nao incluido )
+        return inputName.substring(0,                           // percorre do inicio da string
+                                   inputName.indexOf("."));     // ate a localizacao do ponto ( nao incluido )
+
     }
+
     private static String getDir(Path path) {
         // obtem o caminho absoluto do arquivo ate o diretorio ignorando o arquivo
         return path.toFile()
-                        .getAbsoluteFile()
-                        .getParent();
+                .getAbsoluteFile()
+                .getParent();
     }
+
     private static String extractAuroraName(Path path) {
         // obtem o caminho absoluto, porem so devolve o nome do arquivo com a extensao
         return path.toFile()
-                        .getAbsoluteFile()
-                        .getName();
+                .getAbsoluteFile()
+                .getName();
     }
 
 }
