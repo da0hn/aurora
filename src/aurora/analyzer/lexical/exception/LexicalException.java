6cbab9b38dcc0a7b686724e50d03c8568f3c5bbd
Gabriel Honda <vegancraft13@gmail.com>
2020-03-08 21:26:23 -0400

    Atualização na hierarquia de exceções

--- src/aurora/analyzer/lexical/exception/LexicalException.java
+++ src/aurora/analyzer/lexical/exception/LexicalException.java
@@ -1,11 +1,15 @@
 package aurora.analyzer.lexical.exception;
 
+import aurora.analyzer.exception.AnalyzerException;
+
 /*
  * @project aurora
  * @author Gabriel Honda on 23/02/2020
  */
-public class LexicalException extends RuntimeException {
+public class LexicalException extends AnalyzerException {
+
     public LexicalException(String message) {
         super(message);
     }
+
 }
