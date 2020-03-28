27c11f5d726595a58385c44e12d98473a1c7b5a4
Gabriel Honda <vegancraft13@gmail.com>
2020-03-28 13:54:07 -0400

    Pequena melhoria no Log

--- src/aurora/analyzer/semantic/log/LogSemantic.java
+++ src/aurora/analyzer/semantic/log/LogSemantic.java
@@ -3,6 +3,8 @@ package aurora.analyzer.semantic.log;
 
 import aurora.analyzer.semantic.exception.SemanticException;
 
+import static aurora.parser.Flag.SEMANTIC;
+
 /*
  * @project aurora
  * @author Gabriel Honda on 18/03/2020
@@ -10,7 +12,7 @@ import aurora.analyzer.semantic.exception.SemanticException;
 public class LogSemantic {
 
     public static void log(String msg) {
-        System.out.println(msg);
+        if(SEMANTIC.getValue()) System.out.println(msg);
     }
     public static void error(String err, int line, int column) {
         throw new SemanticException(" at ["+line+", " + column +"]\n\t"+ err);
